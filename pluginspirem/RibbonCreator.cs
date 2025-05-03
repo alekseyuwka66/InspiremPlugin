514ae7b7cc62ce42b53355c83675282b6340f47e
alekseyuwka66 <alex.gegedyush@gmail.com>
2025-05-03 13:25:05 +0300

    feat: added 2 buttons for LightFixteruDimensions and LightFixturePlacement

--- pluginspirem/RibbonCreator.cs
+++ pluginspirem/RibbonCreator.cs
@@ -3,6 +3,7 @@ using Autodesk.Revit.UI;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Windows.Annotations;
 using System.Windows.Media.Imaging;
 
 namespace Inspirem
@@ -32,9 +33,8 @@ namespace Inspirem
 
                 PulldownButton pulldownButton = panel.AddItem(pulldownData) as PulldownButton;
                 pulldownButton.ToolTip = "Инструменты для аннотирования светильников";
-                
-                // Add image to the pulldown button
-                SetButtonImage(pulldownButton, "LightIcon.png");
+
+                SetButtonImage(pulldownButton, "Annotation.png");
 
                 // Add commands to the pull-down button
                 AddPushButton(pulldownButton,
@@ -43,7 +43,7 @@ namespace Inspirem
                     "Inspirem.LightFixtureAnnotationCommand",
                     "Создает аннотацию светильников в выбранном помещении",
                     "Эта команда позволяет выбрать помещение и автоматически создать аннотацию, содержащую количество и типы светильников в этом помещении.",
-                    "LightIcon.png");
+                    "Annotation.png");
 
                 AddPushButton(pulldownButton,
                     "AllLightFixtureAnnotation",
@@ -51,7 +51,23 @@ namespace Inspirem
                     "Inspirem.AllLightFixtureAnnotationCommand",
                     "Создает аннотации светильников для всех помещений",
                     "Автоматически создает аннотации для всех помещений.",
-                    "AllLightsIcon.png");
+                    "Annotation.png");
+
+                AddStandalonePushButton(panel,
+                    "PlaceLightFixtures",
+                    "Расставить светильники",
+                    "Inspirem.PlaceLightFixturesCommand",
+                    "Автоматически расставляет светильники в помещении",
+                    "Эта команда автоматически размещает светильники в выбранном помещении согласно заданным параметрам.",
+                    "Creation.png");
+
+                AddStandalonePushButton(panel,
+                    "PlaceDimensions",
+                    "Расставить размеры",
+                    "Inspirem.PlaceDimensionsCommand",
+                    "Добавляет размеры для светильников",
+                    "Эта команда автоматически добавляет размеры между светильниками и стенами.",
+                    "Dimensions.png");
 
                 return Result.Succeeded;
             }
@@ -100,6 +116,46 @@ namespace Inspirem
             }
 
             var button = pulldownButton.AddPushButton(buttonData);
+        }
+
+        private static void AddStandalonePushButton(RibbonPanel panel,
+                                                  string name,
+                                                  string text,
+                                                  string command,
+                                                  string toolTip,
+                                                  string longDescription,
+                                                  string iconName)
+        {
+            PushButtonData buttonData = new PushButtonData(
+                name,
+                text,
+                thisAssemblyPath,
+                command);
+
+            buttonData.ToolTip = toolTip;
+            buttonData.LongDescription = longDescription;
+
+            // Set the button image
+            try
+            {
+                string iconPath = Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Resources", iconName);
+                if (File.Exists(iconPath))
+                {
+                    buttonData.Image = new BitmapImage(new Uri(iconPath));
+                }
+                else
+                {
+                    // Try to load from embedded resources
+                    Uri uri = new Uri($"pack://application:,,,/{Assembly.GetExecutingAssembly().GetName().Name};component/Resources/{iconName}");
+                    buttonData.Image = new BitmapImage(uri);
+                }
+            }
+            catch
+            {
+                // Use default image if icon loading fails
+            }
+
+            var button = panel.AddItem(buttonData) as PushButton;
         }
 
         private static void SetButtonImage(PulldownButton button, string iconName)
